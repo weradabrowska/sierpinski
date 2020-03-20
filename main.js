d610b3097e1e48ee4ff9487b0184843c25f1b0b8
weradabrowska <weronika.dabrowska@outlook.com>
2020-03-21 00:14:32 +0100

    draw triangle shell

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+function getPoints (iterations) {
+
+};
+
+function getTriangle (iterations) {
+    if(isNaN(iterations) || iterations < 1) {
+        return 'please enter positive integer...';
+    }
+
+    const points = getPoints(iterations);
+
+    const htmlTriangle = '<svg width="400px" height="400px">' +
+    `<polygon points="${points}"` +
+    'style="fill:black;stroke:lime;stroke-width:3;fill-rule:evenodd;" />' +
+  '</svg>'
+    return htmlTriangle;
+};
+
 function draw () {
     const iterationsNumber = document.getElementById('iterations').value;
+    const triangleContainer = document.getElementById('triangle');
+    triangleContainer.innerHTML = getTriangle(iterationsNumber);
 }
